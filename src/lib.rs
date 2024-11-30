b7bc46b39574cb9194d870f6ac5ffdf82d8bf5fa
Hoang Pham <a2k40pbc@gmail.com>
2024-11-30 16:13:01 +0700

    feat: uplift codebase (#1)
    * uplift codebase with all test passed
    
    Signed-off-by: Hoang Pham <a2k40pbc@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,105 @@
-extern crate bincode;
-extern crate rustc_serialize;
-extern crate rand;
+extern crate serde;
 extern crate itertools;
+extern crate rand;
+extern crate bincode;
 
-mod wal_file;
-mod multi_map;
 mod disk_btree;
+mod multi_map;
+mod wal_file;
 
-use wal_file::{KeyValuePair, RecordFile};
-use multi_map::MultiMap;
 use disk_btree::OnDiskBTree;
-
-use rustc_serialize::{Encodable, Decodable};
+use multi_map::MultiMap;
+use wal_file::{KeyValuePair, RecordFile};
 
 use std::error::Error;
 use itertools::merge;
+use serde::{Deserialize, Serialize};
 
 const MAX_MEMORY_ITEMS: usize = 1000;
 
 // specify the types for the keys & values
-pub trait KeyType: Ord + Encodable + Decodable + Clone {}
-pub trait ValueType: Ord + Encodable + Decodable + Clone  {}
+pub trait KeyType: Eq + Ord + Clone + Serialize + for<'de> Deserialize<'de> {}
+pub trait ValueType: Ord + Clone + Serialize + for<'de> Deserialize<'de> {}
 
 // provide generic implementations
 
-impl<T> KeyType for T where T: Ord + Encodable + Decodable + Clone {}
-impl<T> ValueType for T where T: Ord + Encodable + Decodable + Clone {}
+impl<T> KeyType for T where T: Eq + Ord + Clone + Serialize + for<'de> Deserialize<'de> {}
+impl<T> ValueType for T where T: Ord + Clone + Serialize + for<'de> Deserialize<'de> {}
 
 /// This struct holds all the pieces of the BTree mechanism
 pub struct BTree<K: KeyType, V: ValueType> {
     tree_file_path: String,       // the path to the tree file
     key_size: usize,              // the size of the key in bytes
     value_size: usize,            // the size of the value in bytes
-    wal_file: RecordFile<K,V>,    // write-ahead log for in-memory items
-    mem_tree: MultiMap<K,V>,      // in-memory multi-map that gets merged with the on-disk BTree
-    tree_file: OnDiskBTree<K,V>,  // the file backing the whole thing
+    wal_file: RecordFile<K, V>,   // write-ahead log for in-memory items
+    mem_tree: MultiMap<K, V>,     // in-memory multimap that gets merged with the on-disk BTree
+    tree_file: OnDiskBTree<K, V>, // the file backing the whole thing
 }
 
-impl <K: KeyType, V: ValueType> BTree<K, V> {
-    pub fn new(tree_file_path: &String, key_size: usize, value_size: usize) -> Result<BTree<K,V>, Box<Error>> {
-        // create our in-memory multi-map
-        let mut mem_tree = MultiMap::<K,V>::new();
+impl<K: KeyType, V: ValueType> BTree<K, V> {
+    pub fn new(
+        tree_file_path: &String,
+        key_size: usize,
+        value_size: usize,
+    ) -> Result<BTree<K, V>, Box<dyn Error>> {
+        // create our in-memory multimap
+        let mut mem_tree = MultiMap::<K, V>::new();
 
-        // construct the path to the WAL file for the in-memory multi-map
+        // construct the path to the WAL file for the in-memory multimap
         let wal_file_path = tree_file_path.to_owned() + ".wal";
 
         // construct our WAL file
-        let mut wal_file = try!(RecordFile::<K,V>::new(&wal_file_path, key_size, value_size));
+        let mut wal_file = RecordFile::<K, V>::new(&wal_file_path, key_size, value_size)?;
 
         // if we have a WAL file, replay it into the mem_tree
-        if try!(wal_file.is_new()) {
+        if wal_file.is_new()? {
             for kv in &mut wal_file {
                 mem_tree.insert(kv.key, kv.value);
             }
         }
 
         // open the data file
-        let tree_file = try!(OnDiskBTree::<K,V>::new(tree_file_path.to_owned(), key_size, value_size));
+        let tree_file = OnDiskBTree::<K, V>::new(tree_file_path.to_owned(), key_size, value_size)?;
 
-        return Ok(BTree{tree_file_path: tree_file_path.clone(),
-                        key_size: key_size,
-                        value_size: value_size,
-                        tree_file: tree_file,
-                        wal_file: wal_file,
-                        mem_tree: mem_tree});
+        Ok(BTree {
+            tree_file_path: tree_file_path.clone(),
+            key_size,
+            value_size,
+            tree_file,
+            wal_file,
+            mem_tree,
+        })
     }
 
     /// Inserts a key into the BTree
-    pub fn insert(&mut self, key: K, value: V) -> Result<(), Box<Error>> {
-        let record = KeyValuePair{key: key, value: value};
+    pub fn insert(&mut self, key: K, value: V) -> Result<(), Box<dyn Error>> {
+        let record = KeyValuePair { key, value };
 
-        // should wrap this in a read-write lock
-        try!(self.wal_file.insert_record(&record));
+        self.wal_file.insert_record(&record)?;
 
-        let KeyValuePair{key, value} = record;
+        let KeyValuePair { key, value } = record;
 
         let size = self.mem_tree.insert(key, value);
 
         if size > MAX_MEMORY_ITEMS {
-            try!(self.compact());
+            self.compact()?;
         }
 
-        return Ok( () );
+        Ok(())
     }
-
 
     pub fn get(&self, key: &K) -> Option<std::collections::btree_set::Iter<V>> {
         self.mem_tree.get(key).map(|btree| btree)
     }
 
     /// Merges the records on disk with the records in memory
-    fn compact(&mut self) -> Result<(), Box<Error>>{
+    fn compact(&mut self) -> Result<(), Box<dyn Error>> {
         // create a new on-disk BTree
-        let mut new_tree_file = try!(OnDiskBTree::<K,V>::new(self.tree_file_path.to_owned() + ".new", self.key_size, self.value_size));
+        let mut new_tree_file = OnDiskBTree::<K, V>::new(
+            self.tree_file_path.to_owned() + ".new",
+            self.key_size,
+            self.value_size,
+        )?;
 
         // get an iterator for the in-memory items
         let mem_iter = self.mem_tree.into_iter();
@@ -101,27 +108,27 @@ impl <K: KeyType, V: ValueType> BTree<K, V> {
         let disk_iter = self.tree_file.into_iter();
 
         for kv in merge(mem_iter, disk_iter) {
-            try!(new_tree_file.insert_record(&kv));
+            new_tree_file.insert_record(&kv)?;
         }
 
-        Ok( () )
+        Ok(())
     }
 }
-
 
 #[cfg(test)]
 #[allow(unused_must_use)]
 mod tests {
-    use std::fs;
-    use std::fs::OpenOptions;
-    use ::BTree;
     use rand::{thread_rng, Rng};
     use std::collections::BTreeSet;
+    use std::fs;
+    use std::fs::OpenOptions;
+    use rand::distributions::Alphanumeric;
+    use BTree;
 
     pub fn gen_temp_name() -> String {
-        let file_name: String = thread_rng().gen_ascii_chars().take(10).collect();
+        let file_name: String = thread_rng().sample_iter(&Alphanumeric).take(10).map(char::from).collect();
 
-        return String::from("/tmp/") + &file_name + &String::from(".btr");
+        String::from("/tmp/") + &file_name + &String::from(".btr")
     }
 
     fn remove_files(file_path: String) {
@@ -136,18 +143,28 @@ mod tests {
         let btree = BTree::<u8, u8>::new(&file_path, 1, 1).unwrap();
 
         // make sure our two files were created
-        let btf = OpenOptions::new().read(true).write(false).create(false).open(&file_path).unwrap();
-        assert!(btf.metadata().unwrap().len() == 0);
+        let btf = OpenOptions::new()
+            .read(true)
+            .write(false)
+            .create(false)
+            .open(&file_path)
+            .unwrap();
+        assert_eq!(btf.metadata().unwrap().len(), 0);
 
-        let wal = OpenOptions::new().read(true).write(false).create(false).open(file_path.to_owned() + ".wal").unwrap();
-        assert!(wal.metadata().unwrap().len() == 0);
+        let wal = OpenOptions::new()
+            .read(true)
+            .write(false)
+            .create(false)
+            .open(file_path.to_owned() + ".wal")
+            .unwrap();
+        assert_eq!(wal.metadata().unwrap().len(), 0);
 
         // make sure they think they're new too
         assert!(btree.wal_file.is_new().unwrap());
-        assert!(btree.wal_file.count().unwrap() == 0);
+        assert_eq!(btree.wal_file.count().unwrap(), 0);
 
         assert!(btree.tree_file.is_new().unwrap());
-        assert!(btree.tree_file.count().unwrap() == 0);
+        assert_eq!(btree.tree_file.count().unwrap(), 0);
 
         remove_files(file_path); // remove files assuming it all went well
     }
@@ -157,13 +174,15 @@ mod tests {
         let file_path = gen_temp_name();
 
         // scoped so it is cleaned up
-        { BTree::<u8, u8>::new(&file_path, 1, 1).unwrap(); }
+        {
+            BTree::<u8, u8>::new(&file_path, 1, 1).unwrap();
+        }
 
         let btree = BTree::<u8, u8>::new(&file_path, 1, 1).unwrap();
 
         // check our file lengths from the struct
-        assert!(btree.tree_file.count().unwrap() == 0);
-        assert!(btree.wal_file.count().unwrap() == 0);
+        assert_eq!(btree.tree_file.count().unwrap(), 0);
+        assert_eq!(btree.wal_file.count().unwrap(), 0);
 
         remove_files(file_path); // remove files assuming it all went well
     }
@@ -176,7 +195,7 @@ mod tests {
 
         btree.insert(2, 3).unwrap(); // insert into a new file
 
-        assert!(btree.wal_file.count().unwrap() == 1);
+        assert_eq!(btree.wal_file.count().unwrap(), 1);
         assert!(btree.mem_tree.contains_key(&2));
 
         remove_files(file_path); // remove files assuming it all went well
@@ -189,9 +208,11 @@ mod tests {
         let mut btree = BTree::<String, String>::new(&file_path, 15, 15).unwrap();
 
         // insert into a new file
-        btree.insert("Hello".to_owned(), "World".to_owned()).unwrap();
+        btree
+            .insert("Hello".to_owned(), "World".to_owned())
+            .unwrap();
 
-        assert!(! btree.wal_file.is_new().unwrap());
+        assert!(!btree.wal_file.is_new().unwrap());
         assert!(btree.mem_tree.contains_key(&String::from("Hello")));
 
         remove_files(file_path); // remove files assuming it all went well
@@ -225,11 +246,15 @@ mod tests {
         let mut btree = BTree::<String, String>::new(&file_path, 15, 15).unwrap();
 
         // insert into a new file
-        btree.insert("Hello".to_owned(), "World".to_owned()).unwrap();
-        assert!(! btree.wal_file.is_new().unwrap());
+        btree
+            .insert("Hello".to_owned(), "World".to_owned())
+            .unwrap();
+        assert!(!btree.wal_file.is_new().unwrap());
 
-        btree.insert("Hello".to_owned(), "Everyone".to_owned()).unwrap();
-        assert!(! btree.wal_file.is_new().unwrap());
+        btree
+            .insert("Hello".to_owned(), "Everyone".to_owned())
+            .unwrap();
+        assert!(!btree.wal_file.is_new().unwrap());
 
         remove_files(file_path); // remove files assuming it all went well
     }
--- src/multi_map.rs
+++ src/multi_map.rs
@@ -1,27 +1,30 @@
-use ::{KeyType, ValueType};
+use {KeyType, ValueType};
 
 use wal_file::KeyValuePair;
 
-use std::collections::{BTreeMap, BTreeSet};
-use std::collections::btree_map::Entry::Occupied;
 use std::collections::btree_map;
+use std::collections::btree_map::Entry::Occupied;
 use std::collections::btree_set;
 use std::collections::btree_set::Iter;
+use std::collections::{BTreeMap, BTreeSet};
 
 pub struct MultiMap<K: KeyType, V: ValueType> {
     multi_map: BTreeMap<K, BTreeSet<V>>,
-    count: usize  // total number of KV pairs
+    count: usize, // total number of KV pairs
 }
 
 pub struct MultiMapIterator<'a, K: KeyType + 'a, V: ValueType + 'a> {
     cur_key: Option<&'a K>,
-    key_it: btree_map::Iter<'a,K,BTreeSet<V>>,
-    value_it: Option<btree_set::Iter<'a,V>>,
+    key_it: btree_map::Iter<'a, K, BTreeSet<V>>,
+    value_it: Option<btree_set::Iter<'a, V>>,
 }
 
-impl <'a, K: KeyType, V: ValueType> MultiMap<K,V> {
-    pub fn new() -> MultiMap<K,V> {
-        return MultiMap{multi_map: BTreeMap::<K,BTreeSet<V>>::new(), count: 0};
+impl<'a, K: KeyType, V: ValueType> MultiMap<K, V> {
+    pub fn new() -> MultiMap<K, V> {
+        MultiMap {
+            multi_map: BTreeMap::<K, BTreeSet<V>>::new(),
+            count: 0,
+        }
     }
 
     pub fn insert(&mut self, key: K, value: V) -> usize {
@@ -31,14 +34,14 @@ impl <'a, K: KeyType, V: ValueType> MultiMap<K,V> {
             set.insert(value);
             return self.count;
         }
-        
+
         let mut set = BTreeSet::<V>::new();
 
         set.insert(value);
 
         self.multi_map.insert(key, set);
 
-        return self.count;
+        self.count
     }
 
     /*
@@ -47,13 +50,13 @@ impl <'a, K: KeyType, V: ValueType> MultiMap<K,V> {
      * sure how: https://goo.gl/9sisAb
      */
     pub fn get(&self, key: &K) -> Option<Iter<V>> {
-        return self.multi_map.get(key).map(|set| set.iter());
+        self.multi_map.get(key).map(|set| set.iter())
     }
 
     pub fn contains_key(&self, key: &K) -> bool {
         match self.get(key) {
             Some(_) => true,
-            None => false
+            None => false,
         }
     }
 
@@ -64,9 +67,8 @@ impl <'a, K: KeyType, V: ValueType> MultiMap<K,V> {
      */
     pub fn delete(&mut self, key: K, value: V) -> usize {
         if let Occupied(mut entry) = self.multi_map.entry(key) {
-
             if entry.get_mut().remove(&value) {
-                self.count -= 1;            
+                self.count -= 1;
             }
 
             if entry.get().is_empty() {
@@ -74,17 +76,17 @@ impl <'a, K: KeyType, V: ValueType> MultiMap<K,V> {
             }
         }
 
-        return self.count;
+        self.count
     }
 
     pub fn size(&self) -> usize {
-        return self.count;
+        self.count
     }
 }
 
-impl <'a, K: KeyType, V: ValueType> IntoIterator for &'a mut MultiMap<K,V> {
-    type Item = KeyValuePair<K,V>;
-    type IntoIter = MultiMapIterator<'a,K,V>;
+impl<'a, K: KeyType, V: ValueType> IntoIterator for &'a mut MultiMap<K, V> {
+    type Item = KeyValuePair<K, V>;
+    type IntoIter = MultiMapIterator<'a, K, V>;
 
     fn into_iter(self) -> Self::IntoIter {
         let mut key_it = self.multi_map.iter();
@@ -92,23 +94,31 @@ impl <'a, K: KeyType, V: ValueType> IntoIterator for &'a mut MultiMap<K,V> {
 
         // check to see if our map is empty
         if cur_entry.is_none() {
-            return MultiMapIterator{cur_key: None, key_it: key_it, value_it: None};
+            return MultiMapIterator {
+                cur_key: None,
+                key_it: key_it,
+                value_it: None,
+            };
         }
 
         // safe to call unwrap as we tested above
         let (cur_key, cur_set) = cur_entry.unwrap();
 
-        return MultiMapIterator{cur_key: Some(cur_key), key_it: key_it, value_it: Some(cur_set.iter())};
+        MultiMapIterator {
+            cur_key: Some(cur_key),
+            key_it: key_it,
+            value_it: Some(cur_set.iter()),
+        }
     }
 }
 
-impl <'a, K: KeyType, V: ValueType> Iterator for MultiMapIterator<'a,K,V> {
-    type Item = KeyValuePair<K,V>;
+impl<'a, K: KeyType, V: ValueType> Iterator for MultiMapIterator<'a, K, V> {
+    type Item = KeyValuePair<K, V>;
 
     fn next(&mut self) -> Option<Self::Item> {
         // this is our invariant, when it's None we've gone through everything
         if self.cur_key.is_none() {
-            return None
+            return None;
         }
 
         // should be safe to call unwrap here, because we checked for None above
@@ -131,10 +141,12 @@ impl <'a, K: KeyType, V: ValueType> Iterator for MultiMapIterator<'a,K,V> {
             cur_val = self.value_it.as_mut().unwrap().next(); // set our current value
         }
 
-        return Some(KeyValuePair{key: self.cur_key.unwrap().clone(), value: cur_val.unwrap().clone()});
+        Some(KeyValuePair {
+            key: self.cur_key.unwrap().clone(),
+            value: cur_val.unwrap().clone(),
+        })
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -142,68 +154,66 @@ mod tests {
 
     #[test]
     fn test_insert() {
-        let mut mmap = MultiMap::<i32,String>::new();
+        let mut mmap = MultiMap::<i32, String>::new();
 
-        assert!(mmap.insert(12, String::from("abc")) == 1);
-        assert!(mmap.insert(23, String::from("abc")) == 2);
-        assert!(mmap.insert(23, String::from("def")) == 3);
+        assert_eq!(mmap.insert(12, String::from("abc")), 1);
+        assert_eq!(mmap.insert(23, String::from("abc")), 2);
+        assert_eq!(mmap.insert(23, String::from("def")), 3);
 
         let mut it = mmap.into_iter();
 
         let e1 = it.next().unwrap();
-        assert!(12 == e1.key);
-        assert!(String::from("abc") == e1.value);
-        
+        assert_eq!(12, e1.key);
+        assert_eq!(String::from("abc"), e1.value);
+
         let e2 = it.next().unwrap();
-        assert!(23 == e2.key);
-        assert!(String::from("abc") == e2.value);
-        
+        assert_eq!(23, e2.key);
+        assert_eq!(String::from("abc"), e2.value);
+
         let e3 = it.next().unwrap();
-        assert!(23 == e3.key);
-        assert!(String::from("def") == e3.value);
+        assert_eq!(23, e3.key);
+        assert_eq!(String::from("def"), e3.value);
     }
 
     #[test]
     fn test_get() {
-        let mut mmap = MultiMap::<i32,String>::new();
-        
-        assert!(mmap.insert(12, String::from("abc")) == 1);
-        assert!(mmap.insert(23, String::from("abc")) == 2);
-        assert!(mmap.insert(23, String::from("def")) == 3);
+        let mut mmap = MultiMap::<i32, String>::new();
+
+        assert_eq!(mmap.insert(12, String::from("abc")), 1);
+        assert_eq!(mmap.insert(23, String::from("abc")), 2);
+        assert_eq!(mmap.insert(23, String::from("def")), 3);
 
         let mut it1 = mmap.get(&12).unwrap();
 
-        assert!(it1.next().unwrap() == "abc");
-        assert!(it1.next() == None);
+        assert_eq!(it1.next().unwrap(), "abc");
+        assert_eq!(it1.next(), None);
 
         let mut it2 = mmap.get(&23).unwrap();
 
-        assert!(it2.next().unwrap() == "abc");
-        assert!(it2.next().unwrap() == "def");
-        assert!(it2.next() == None);
+        assert_eq!(it2.next().unwrap(), "abc");
+        assert_eq!(it2.next().unwrap(), "def");
+        assert_eq!(it2.next(), None);
     }
 
     #[test]
     fn test_delete() {
-        let mut mmap = MultiMap::<i32,String>::new();
+        let mut mmap = MultiMap::<i32, String>::new();
 
-        assert!(mmap.insert(12, String::from("abc")) == 1);
-        assert!(mmap.insert(23, String::from("abc")) == 2);
-        assert!(mmap.insert(23, String::from("def")) == 3);
+        assert_eq!(mmap.insert(12, String::from("abc")), 1);
+        assert_eq!(mmap.insert(23, String::from("abc")), 2);
+        assert_eq!(mmap.insert(23, String::from("def")), 3);
 
-        assert!(mmap.size() == 3);
+        assert_eq!(mmap.size(), 3);
 
-        assert!(mmap.delete(12, String::from("abc")) == 2);
-        assert!(mmap.delete(23, String::from("abc")) == 1);
-        assert!(mmap.delete(23, String::from("abc")) == 1); // should NOT find this one
-        assert!(mmap.delete(23, String::from("def")) == 0);
+        assert_eq!(mmap.delete(12, String::from("abc")), 2);
+        assert_eq!(mmap.delete(23, String::from("abc")), 1);
+        assert_eq!(mmap.delete(23, String::from("abc")), 1); // should NOT find this one
+        assert_eq!(mmap.delete(23, String::from("def")), 0);
 
-        assert!(mmap.size() == 0);
+        assert_eq!(mmap.size(), 0);
 
         let mut it = mmap.into_iter();
 
         assert!(it.next() == None);
     }
 }
-
-
